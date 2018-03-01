8b5b3a9752919c90e4cc6be116b58a04c36866ef
jgarabedian <jgarabedian@USARRJGARABEDI1.us.deloitte.com>
2018-03-01 17:01:33 -0500

    Commited on..  <d>2018/03/01 17:01:33

---
+++ 2b67de77-9774-4ca8-8f4b-db8dfa737f44/32e43256-82aa-4d4e-82d9-4d80d73da624_2018-03-01-12-30-33_2018-03-01-12-30-33.c
@@ -0,0 +1,8 @@
+{
+  "action": "add",
+  "comment": "Rates are shown as % of total labor pop.",
+  "createdTimeStamp": "2018-03-01-12-30-33",
+  "engine": "happyCountryDB",
+  "id": "32e43256-82aa-4d4e-82d9-4d80d73da624_2018-03-01-12-30-33",
+  "insightId": "2b67de77-9774-4ca8-8f4b-db8dfa737f44"
+}
